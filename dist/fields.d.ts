b6c18c6c985c6cfe92e25619e803ecd0e40816af
alexrr2iggs <alexandru.bunduche@yahoo.com>
2022-07-01 18:33:59 +0200

    fix

--- dist/fields.d.ts
+++ dist/fields.d.ts
@@ -1,3 +1,3 @@
 import { PartialDeep, Schema } from 'type-fest';
-export declare type Fields<T> = Schema<PartialDeep<T>, boolean | T> | boolean;
+export declare type Fields<T> = Schema<PartialDeep<T>, boolean> | boolean;
 //# sourceMappingURL=fields.d.ts.map
--- src/fields.ts
+++ src/fields.ts
@@ -1,3 +1,3 @@
 import { PartialDeep, Schema } from 'type-fest';
 
-export type Fields<T> = Schema<PartialDeep<T>, boolean | T> | boolean;
+export type Fields<T> = Schema<PartialDeep<T>, boolean> | boolean;
