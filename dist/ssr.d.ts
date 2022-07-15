559ee8d28dae1a4cadcf0a97515d7b8e47eb6591
alexrr2iggs <alexandru.bunduche@yahoo.com>
2022-07-15 10:04:22 +0200

    sd

---
+++ dist/ssr.d.ts
@@ -0,0 +1,21 @@
+import { PaginatedResult } from './paginated-result';
+import { PaginatedSearch } from './paginated-search';
+import { Search } from './search-operators';
+export interface SSR<T> {
+    /**
+     * create or update one or more entities
+     * @param e
+     */
+    save(...e: T[]): Promise<T[]>;
+    /**
+     * perform a paginated and ordered search on entities
+     * @param search
+     */
+    search(search: PaginatedSearch<T>): Promise<PaginatedResult<T>>;
+    /**
+     * delete entities matching the search parameters
+     * @param search
+     **/
+    remove(search: Search<T>): Promise<T[]>;
+}
+//# sourceMappingURL=ssr.d.ts.map
