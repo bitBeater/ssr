6bbd9c1fbbfed31d9ddda15f785cd5f401cccb6d
alexrr2iggs <alexandru.bunduche@yahoo.com>
2022-10-13 13:23:21 +0200

    s

--- dist/ssr.d.ts
+++ dist/ssr.d.ts
@@ -1,22 +1,38 @@
 import { PaginatedResult } from './paginated-result';
 import { PaginatedSearch } from './paginated-search';
 import { Search } from './search-operators';
+/**
+ * create or update one or more entities
+ * @param e
+ */
+export declare type ssrSaveFn<T> = (e: T[]) => Promise<T[]>;
+/**
+ * perform a paginated and ordered search on entities
+ * if search is not provided, all entities are returned
+ * @param search
+ */
+export declare type ssrSearchFn<T> = (search?: PaginatedSearch<T>) => Promise<PaginatedResult<T>>;
+/**
+ * delete entities matching the search parameters
+ * @param search
+ **/
+export declare type ssrRemoveFn<T> = (search: Search<T>) => Promise<T[]>;
 export interface SSR<T> {
     /**
      * create or update one or more entities
      * @param e
      */
-    save(e: T[]): Promise<T[]>;
+    save: ssrSaveFn<T>;
     /**
      * perform a paginated and ordered search on entities
      * if search is not provided, all entities are returned
      * @param search
      */
-    search(search?: PaginatedSearch<T>): Promise<PaginatedResult<T>>;
+    search: ssrSearchFn<T>;
     /**
      * delete entities matching the search parameters
      * @param search
      **/
-    remove(search: Search<T>): Promise<T[]>;
+    remove: ssrRemoveFn<T>;
 }
 //# sourceMappingURL=ssr.d.ts.map
