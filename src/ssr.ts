9f4c3bf62fe7265060715cafb46e9622e1fa1055
alexrr2iggs <alexandru.bunduche@yahoo.com>
2022-10-13 13:22:40 +0200

    dfg

--- src/ssr.ts
+++ src/ssr.ts
@@ -2,23 +2,45 @@ import { PaginatedResult } from './paginated-result';
 import { PaginatedSearch } from './paginated-search';
 import { Search } from './search-operators';
 
+
+
+/**
+ * create or update one or more entities
+ * @param e
+ */
+export type ssrSaveFn<T> = (e: T[]) => Promise<T[]>;
+
+/**
+ * perform a paginated and ordered search on entities
+ * if search is not provided, all entities are returned
+ * @param search
+ */
+export type ssrSearchFn<T> = (search?: PaginatedSearch<T>) => Promise<PaginatedResult<T>>;
+
+/**
+ * delete entities matching the search parameters
+ * @param search
+ **/
+export type ssrRemoveFn<T> = (search: Search<T>) => Promise<T[]>;
+
+
 export interface SSR<T> {
 	/**
 	 * create or update one or more entities
 	 * @param e
 	 */
-	save(e: T[]): Promise<T[]>;
+	save: ssrSaveFn<T>;
 
 	/**
 	 * perform a paginated and ordered search on entities
 	 * if search is not provided, all entities are returned
 	 * @param search
 	 */
-	search(search?: PaginatedSearch<T>): Promise<PaginatedResult<T>>;
+	search: ssrSearchFn<T>;
 
 	/**
 	 * delete entities matching the search parameters
 	 * @param search
 	 **/
-	remove(search: Search<T>): Promise<T[]>;
+	remove: ssrRemoveFn<T>;
 }
